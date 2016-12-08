841b319f3320b4504a49626610fb47da7ec923de
Stefano Borini <sborini@enthought.com>
2016-12-08 14:38:32 +0000

    Fixed appveyor run

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -1,6 +1,9 @@
 "%sdkverpath%" -q -version:"%sdkver%"
 call setenv /x64
 
+pip install -r dev_requirements.txt
+pip install -r doc_requirements.txt
+
 flake8 .
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage run -m unittest discover -v -p test*
