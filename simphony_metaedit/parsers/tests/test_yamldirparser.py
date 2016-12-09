497e1da09af2994e395618d4b17eefb9aafb966d
Stefano Borini <sborini@enthought.com>
2016-12-09 14:22:54 +0000

    Adds a parser class for the files

---
+++ simphony_metaedit/parsers/tests/test_yamldirparser.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+from simphony_metaedit.parsers.yamldirparser import YamlDirParser
+from simphony_metaedit import nodes
+
+
+class TestYamlDirParser(unittest.TestCase):
+    def setUp(self):
+        self.yamldir = os.path.join(
+            os.path.dirname(__file__),
+            'fixtures',
+            'yaml_files')
+
+    def test_initialization(self):
+        parser = YamlDirParser()
+
+        root = parser.parse(self.yamldir)
+        self.assertEqual(type(root), nodes.RootNode)
+
+        self.assertEqual(type(root), nodes.RootNode)
+
